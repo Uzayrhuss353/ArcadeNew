26b3dde9394331403f89b2673cf46e12b8ac5dce
Sanjanakumaran05@gmail.com <Sanjanakumaran05@gmail.com>
2022-01-28 10:37:23 -0500

    Start GAMEE

--- CulminatingAssignment/ArcadeGames/frmMain.Designer.cs
+++ CulminatingAssignment/ArcadeGames/frmMain.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StartGame = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // StartGame
+            // 
+            this.StartGame.Location = new System.Drawing.Point(260, 123);
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(462, 205);
+            this.StartGame.TabIndex = 0;
+            this.StartGame.Text = "Start Game!!!!";
+            this.StartGame.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.StartGame);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartGame;
     }
 }
 
